79312be9c76361d066a0919d543fc7680a575c2f
John Kane <john@kanej.me>
2015-06-05 19:38:25 +0100

    Added in the Clifford attractors.

--- src/ifs/core.clj
+++ src/ifs/core.clj
@@ -16,6 +16,11 @@
      (- (q/sin (* c x))
         (q/cos (* d y)))]))
 
+(defn clifford-is [a b c d]
+  (fn [[x y]]
+    [(+ (q/sin (* a y)) (* c (q/cos (* a x))))
+     (+ (q/sin (* b x)) (* d (q/cos (* b y))))]))
+
 (def scale-factor 200)
 
 (defn scale-up [[x y]]
@@ -62,16 +67,25 @@
     ; fun-mode.
     :middleware [m/fun-mode]))
 
+;; De Jong
+
 ;;(sketch-ifs (dejong-is 0.97 -1.9 1.38 -1.5))
 ;;(sketch-ifs (dejong-is 1.641 1.902 0.316 1.525))
 ;;(sketch-ifs (dejong-is 0.970 -1.899 1.381 -1.506))
-(sketch-ifs (dejong-is 1.4 -2.3 2.4 -2.1))
+;;(sketch-ifs (dejong-is 1.4 -2.3 2.4 -2.1))
 ;;(sketch-ifs (dejong-is 2.01 -2.53 1.61 -0.33))
 ;;(sketch-ifs (dejong-is -2.7 -0.09 -0.86 -2.2))
 ;;(sketch-ifs (dejong-is -2.24 0.43 -0.65 -2.43))
 ;;(sketch-ifs (dejong-is -2 -2 -1.2 2))
 ;;(sketch-ifs (dejong-is -0.709 1.638 0.452 1.740))
 
-
+;; Clifford
+;;(sketch-ifs (clifford-is -1.4 1.6 1.0 0.7))
+;;(sketch-ifs (clifford-is 1.6 -0.6 -1.2 1.6))
+;;(sketch-ifs (clifford-is 1.7 1.7 0.6 1.2))
+(sketch-ifs (clifford-is 1.5 -1.8 1.6 0.9))
+;;(sketch-ifs (clifford-is -1.7 1.3 -0.1 -1.2))
+;;(sketch-ifs (clifford-is -1.7 1.8 -1.9 -0.4))
+;;(sketch-ifs (clifford-is -1.8 -2.0 -0.5 -0.9))
 
 ;;(sketch-ifs s-is)
